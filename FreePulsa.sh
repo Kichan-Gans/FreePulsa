e724a9905f17811a12b195956aa414365f9d1a8f
Kichan-Gans <65236060+Kichan-Gans@users.noreply.github.com>
2020-05-14 07:39:11 +0700

    Create FreePulsa.sh

---
+++ FreePulsa.sh
@@ -0,0 +1,139 @@
+#!/system/xbin/bash
+#Bersihkan Layar
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 1
+echo "\033[32;1m=
+
+
+  __________________________
+  | $ Pulsa Gratis         |
+  | $ BY Ferdi Gans        |
+  | $ wa 6289529837692     |
+  | $ subs bl4ck kich4n    |
+  |________________________|
+     _____||_______||____                 
+     |__________________|
+
+"
+toilet -f standard -F gay "Ferdi Gans"
+echo "\033[31;1m=
+
+--------------------------------
+    Made By Ferdi Gans ea
+    Subs Bl4ck Kich4n
+    ig @ferdi29._
+--------------------------------
+
+"
+echo ""
+echo "By : " Ferdi Gans"
+" YT : " Bl4ck Kich4n"
+echo " wa : " 6289529837692"
+" Pesan : "Hanya Work Untuk Hp Yg Sudah Di Root:)"
+sleep 1
+echo "ThanksTo:"
+echo "Ferdi Gans Was Here"
+echo "\033[32;1m=
+  __________________________
+  | $ Pulsa Gratis         |
+  | $ BY Ferdi Gans        |
+  | $ WA 6289529837692     |
+  | $ Subs Bl4ck Kich4n    |
+  |________________________|
+     _____||_______||____                 
+     |__________________|
+"
+echo ""
+sleep 3
+echo "Hanya Bisa Buat Hp Yg Udh Di Root ya"
+echo " Pakai +62 Ya Beb#Contoh +62xxx:"
+read p;
+
+echo "Subs Bl4ck Kich4n"
+sleep 2
+echo "\033[37;1mANDA  10%" 
+sleep 1
+echo "G  20%" 
+sleep 1
+echo "O  30%" 
+sleep 3
+echo "B  40%" 
+sleep 3
+echo "L  50%" 
+sleep 3
+echo "O  60%"
+echo "K  90%"
+sleep 5
+echo "\033[31;1mNomor Gagal Beb:(...""\033[32;1mSilahkan Masukkan Kembali Nomor Kamu:"
+read p;
+sleep 3
+echo "Subs Bl4ck Kich4n..."
+sleep 3
+echo "\033[37;1m#10%"
+sleep 2
+echo "##20%"
+sleep 5
+echo "###30%"
+sleep 3
+echo "####40%"
+sleep 3
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 3
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 3
+echo "\033[32;1mBerapa Pulsa Yang Lu Mau(max 20k ya jgn rakus) :"
+read p;
+sleep 2
+echo "Subs Bl4ck Kich4n..."
+echo "\033[37;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 3
+echo "####40%"
+sleep 3
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 3
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 6
+echo "\033[34;1mSukses Beb...""\033[32;1m Tunggu 15/30 Menit Beb..."
+sleep 2
+sleep 3
+echo "Ketik Exit Untuk Keluar"
+read p;
+sleep 2
+sleep 3
+sleep 4
+
+echo "Pesan:Jika Script Ini Tidak Berfungsi Pada Anda Berarti Anda Belum Di Root"
+sleep 3
+echo "Selamat Kalau PulsaNya Masuk Berarti Hp Anda Sudah Di Root + Hoki"
+sleep 2
+echo "ThanksTo Ferdi Gans/Bl4ck Kich4n"
+sleep 2
+sleep 4
+exit
